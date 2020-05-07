fb0f1ce62c0d021aa0e974242262833b957629c8
Paulo Lopes <silvavitorlopes711@gmail.com>
2020-05-07 17:45:20 -0300

    chore: using Node fs

--- instructors.js
+++ instructors.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 exports.post =  function(req, res){
     const keys = Object.keys(req.body)
 
@@ -6,5 +8,12 @@ exports.post =  function(req, res){
             res.send("Por favor, preencha todos os campos!")
         }
     }
+
+    fs.writeFile("data.json", JSON.stringify(req.body), function(err){
+        if(err) return res.send("Write file error!")
+
+        return res.redirect("/instructors")
+    })
+
     return res.send(req.body)
 }
