9edfd7023135feab8edb37bde9f3557af97c15d9
Paulo Lopes <silvavitorlopes711@gmail.com>
2020-05-18 15:31:10 -0300

    add: create edit function

--- instructors.js
+++ instructors.js
@@ -59,3 +59,19 @@ exports.post =  function(req, res){
         return res.redirect("/instructors")
     })
 }
+
+
+//Edit
+
+exports.edit = function(req, res){
+    const {id} = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found!")
+
+
+    return res.render('instructors/edit', {instructor: foundInstructor})
+}
